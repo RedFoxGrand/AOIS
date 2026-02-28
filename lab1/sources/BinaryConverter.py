281fd43401c17578aff7fc8dce94c12ed859304b
RedFoxGrand <nik450268@gmail.com>
2026-02-28 12:13:18 +0300

    Update BinaryConverter.py

--- lab1/sources/BinaryConverter.py
+++ lab1/sources/BinaryConverter.py
@@ -1,11 +1,12 @@
 from sources.BasicFunctions import BasicFunctions
+from sources.BasicFunctions import LENGTH_OF_BITS
 
 
 class BinaryConverter:
     @staticmethod
     def converting_to_sign_magnitude(input_number: str) -> list[int]:
         number = int(input_number)
-        bits = BasicFunctions.int_to_bits(abs(number), 32)
+        bits = BasicFunctions.int_to_bits(abs(number), LENGTH_OF_BITS)
         if number < 0:
             bits[0] = 1
         return bits
@@ -13,7 +14,7 @@ class BinaryConverter:
     @staticmethod
     def converting_to_ones_complement(input_number: str) -> list[int]:
         number = int(input_number)
-        bits = BasicFunctions.int_to_bits(abs(number), 32)
+        bits = BasicFunctions.int_to_bits(abs(number), LENGTH_OF_BITS)
         if number < 0:
             for i in range(len(bits)):
                 bits[i] = 1 if bits[i] == 0 else 0
@@ -27,7 +28,7 @@ class BinaryConverter:
             return bits
 
         carry = 1
-        for i in range(31, -1, -1):
+        for i in range(LENGTH_OF_BITS - 1, -1, -1):
             sum = bits[i] + carry
             bits[i] = sum % 2
             carry = sum // 2
